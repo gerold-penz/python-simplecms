350e8b1f94206d44fa6e3816bf758c0d238f9b74
Gerold Penz <gerold@halvar.at>
2013-02-18 21:28:18 +0100

    Dieser Branch enthält jetzt die mit Sphinx erstellten HTML-Dateien

---
+++ searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({objects:{},terms:{index:0,search:0,welcom:0,python:0,modul:0,indic:0,content:0,tabl:0,document:0,simpl:0,page:0},objtypes:{},titles:["Welcome to Simple Python CMS&#8217;s documentation!"],objnames:{},filenames:["index"]})
